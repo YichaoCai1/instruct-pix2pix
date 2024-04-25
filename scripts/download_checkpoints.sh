02cbab0187e8390e1805251c6c82ffc1e2000651
Yichao Cai <yichao.cai@adelaide.edu.au>
2024-04-25 23:23:32 +0930

    update

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-mkdir -p $SCRIPT_DIR/../checkpoints
-
-curl http://instruct-pix2pix.eecs.berkeley.edu/instruct-pix2pix-00-22000.ckpt -o $SCRIPT_DIR/../checkpoints/instruct-pix2pix-00-22000.ckpt
+mkdir -p ./checkpoints
+curl http://instruct-pix2pix.eecs.berkeley.edu/instruct-pix2pix-00-22000.ckpt -o ./checkpoints/instruct-pix2pix-00-22000.ckpt
